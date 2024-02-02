16fd2c91dfc200da558a19c68e071346a20cb28f
HybridMichal <118260010+HybridMichal@users.noreply.github.com>
2024-02-02 19:03:10 +0100

    shopping list

---
+++ shopping-list/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <h1>Shopping List</h1>
+    <input v-model="newItem" placeholder="Enter an item">
+    <button @click="addItem">Add</button>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+        <button @click="removeItem(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      newItem: '',
+      items: []
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.newItem.trim() !== '') {
+        this.items.push(this.newItem);
+        this.newItem = '';
+      }
+    },
+    removeItem(index) {
+      this.items.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
